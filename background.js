4b789c72db56f03ab2a5740fbd5a009494bc6568
shaiyon <hshaiyon@gmail.com>
2024-04-08 15:41:54 -0700

    optimize for mobile, and add auto scroll for mobile

--- background.js
+++ background.js
@@ -1,53 +1,80 @@
-function isMobileDevice() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+function isMobileView() {
+    return window.innerWidth <= 768; // Adjust as necessary for your design's breakpoint
 }
 
 const images = [
-    { path: '/assets/tiles/Abstract_pattern_22.svg', size: '600px' },
+    { path: '/assets/tiles/Abstract_pattern_22.svg', size: 600 },
     // { path: '/assets/tiles/13_06_jxxxj.svg', size: '450px' },
-    { path: '/assets/tiles/5_SlVOXzEwLTEz.svg', size: '400px' },
-    { path: '/assets/tiles/10_1.svg', size: '350px' },
+    { path: '/assets/tiles/5_SlVOXzEwLTEz.svg', size: 400 },
+    { path: '/assets/tiles/10_1.svg', size: 400 },
 ];
 
-var selectedBackgroundImage = null;
-function setRandomBackgroundImage() {
+let backgroundImageHeight = 0;
+let scrollPosition = 0;
+const speed = 0.5; 
+
+function scrollBackground() {
+    if (backgroundImageHeight > 0) { 
+        scrollPosition -= speed;
+
+        if (Math.abs(scrollPosition) >= backgroundImageHeight) {
+            scrollPosition += backgroundImageHeight;
+        }
+        const parallaxBackground = document.getElementById('parallaxBackground');
+        parallaxBackground.style.transform = `translateY(${scrollPosition}px)`;
+    }
+
+    requestAnimationFrame(scrollBackground);
+}
+
+function set_loader() {
+    const loader = document.createElement('div');
+    loader.id = 'loader';
+    loader.style.position = 'fixed';
+    loader.style.top = '0';
+    loader.style.left = '0';
+    loader.style.width = '100%';
+    loader.style.height = '100%';
+    loader.style.display = 'flex';
+    loader.style.alignItems = 'center';
+    loader.style.justifyContent = 'center';
+    loader.style.backgroundColor = '#000';
+    loader.innerHTML = '<div class="dot-spin">Loading...</div>';
+    document.body.appendChild(loader);    
+}
+
+function setRandomBackgroundImage(isMobileView) {
     const randomIndex = Math.floor(Math.random() * images.length);
     const imageObject = images[randomIndex];
-    selectedBackgroundImage = imageObject;
 
     const tempImage = new Image();
     tempImage.src = imageObject.path;
     tempImage.onload = function() {
-        // Once the background image is loaded, display the content and remove the loading indicator
+        backgroundImageHeight = imageObject.size
         document.getElementById('parallaxBackground').style.display = 'block';
         document.body.style.background = 'none'; // Remove black background
+        loader = document.getElementById('loader');
+        document.body.removeChild(loader);
+        showText("#desc", "Software Engineer", 0, 100);
     };
+
+    var scalar = isMobileView ? 0.5 : 1;
 
     const parallaxBackground = document.getElementById('parallaxBackground');
     parallaxBackground.style.backgroundImage = `url('${imageObject.path}')`;
-    parallaxBackground.style.backgroundSize = `${imageObject.size} ${imageObject.size}`;
+    parallaxBackground.style.backgroundSize = `${imageObject.size*scalar}px ${imageObject.size*scalar}px`;
     parallaxBackground.style.backgroundRepeat = 'repeat';
-    parallaxBackground.style.backgroundPosition = 'center'; // Center the background image
-}
-
-function throttle(callback, limit) {
-    var waiting = false;
-    return function () {
-        if (!waiting) {
-        callback.apply(this, arguments);
-        waiting = true;
-        setTimeout(function () {
-            waiting = false;
-        }, limit);
-        }
+    parallaxBackground.style.backgroundPosition = 'center';
+    if (isMobileView) {
+        parallaxBackground.style.height = '200%';
     }
 }
+
 let lastX = 0;
 let lastY = 0;
 let targetX = 0;
 let targetY = 0;
 let isMoving = false;
-
 const applyParallaxEffect = () => {
     // Calculate the current offset based on last and target positions
     const currentX = lastX + (targetX - lastX) * 0.1; // Smooth transition factor
@@ -71,9 +98,10 @@ const applyParallaxEffect = () => {
 
 
 window.onload = function() {
-    setRandomBackgroundImage();
+    set_loader();
+    setRandomBackgroundImage(isMobileView());
 
-    if (!isMobileDevice()) {
+    if (!isMobileView()) {
         document.addEventListener('mousemove', function(e) {
             // Update target positions based on mouse location
             targetX = (e.clientX - window.innerWidth / 2) / 15;
@@ -85,5 +113,8 @@ window.onload = function() {
                 requestAnimationFrame(applyParallaxEffect);
             }
         });
+    }
+    else {
+        scrollBackground();
     }
 };
